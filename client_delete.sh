67fc7da3aeb7a74c8c02db8c3fcc77fa662dbc18
honey data <honeydatax@sapo.pt>
2017-03-20 20:40:17 +0000

    free code

---
+++ client_delete.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+clear 
+printf "name to find:?" 
+read toFind
+printf "%s" $toFind > clientfinder.txt
+fgrep -f clientfinder.txt client.txt
+
+printf "delete client y:?" 
+read toFind
+if [ "$toFind" = "n" ] ;
+then exit
+fi
+if [ "$toFind" = "N" ] ;
+then exit
+fi
+printf "client id to delete:?" 
+read toFind
+printf "%s" " $toFind :" > clientfinder.txt
+fgrep -f clientfinder.txt client.txt
+printf "delete client y:?" 
+read toFinds
+if [ "$toFinds" = "n" ] ;
+then exit
+fi 
+
+if [ "$toFinds" = "N" ] ;
+then exit
+fi
+
+printf ". $toFind :.\n"
+grep -i -v " $toFind :"  client.txt > clientdelete.txt
+
+
+printf "client delete:\n" 
+printf "$toFind" 
+
+sudo cp clientdelete.txt client.txt
+
+read nnull
+
+
+
+
+
+
+
