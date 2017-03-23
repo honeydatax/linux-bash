bf25eef16a8c5bb31a4351e928e9352a3ed2f965
honey data <honeydatax@sapo.pt>
2017-03-23 11:20:35 +0000

    Update random.sh

--- random.sh
+++ random.sh
@@ -2,5 +2,4 @@
 
 clear
 
-awk -F: 'BEGIN{srand();i=0;x=0;for (i=0;i<=11;i++){x=rand()*50+1;printf("%d \n",x);}}' ccalc.txt 
-
+awk -F: 'BEGIN{srand();i=0;x=0;for (i=0;i<=11;i++){x=rand()*50+1;printf("%d \n",x);}}'
