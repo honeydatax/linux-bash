94a6ea72181a9d1e926755a9e271be0086fbb009
honey data <honeydatax@sapo.pt>
2017-03-23 11:19:14 +0000

    free code

--- ccalc.sh
+++ ccalc.sh
@@ -13,7 +13,9 @@ printf ":"  $tinfo >> ccalc.txt
 printf "%10s "  $tvalue >> ccalc.txt
 printf "\n"  >> ccalc.txt
 
-awk -F: 'BEGIN{value=0;}{value = value + $2 ; print $1 , "\t" , $2 , "\t" , value ; }END{print "total:\t",value}' 
+awk -F: 'BEGIN{value=0;}{value = value + $2 ; print $1 , "\t" , $2 , "\t" , value ; }END{print "total:\t",value}' ccalc.txt
+
+
 printf ".....................\n"
 
 
