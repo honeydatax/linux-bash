8a2000bb06e7f5736884c98c94f42609cf6bfaef
honey data <honeydatax@sapo.pt>
2017-05-01 22:10:08 +0000

    free code cript strings to hex

--- hide.sh
+++ hide.sh
@@ -1,33 +1,30 @@
-#!/bin/bash
+#!/bin/bs
 
 if [ "$1" == "" ]
 then 
-printf "%s cript %s decript\n\n" "-c" "-d"
+printf "%s cript %s decript\n\n" "-c" "-d" > out.txt
+zenity --text-info --filename="out.txt" --title="cript decript" &
 exit
 fi
 
 if [ "$1" == "-c" ]
 then 
-printf "\nstring to cript: "
-read nnull
-printf "\n"
+nnull=$(zenity --entry --title="enter string to cript")
 printf "" > hide.txt
 printf "%s" "$nnull" >> hide.txt
-xxd -p hide.txt
-printf "\n"
+xxd -p hide.txt > out.txt
+zenity --text-info --filename="out.txt" --title="string cript" &
 exit
 fi
 
 
 if [ "$1" == "-d" ]
 then 
-printf "\nstring to decript: "
-read nnull
-printf "\n"
+nnull=$(zenity --entry --title="enter string to cript")
 printf "" > hide.txt
 printf "%s" "$nnull" >> hide.txt
-xxd -p -r hide.txt
-printf "\n"
+xxd -p -r hide.txt > out.txt
+zenity --text-info --filename="out.txt" --title="string decript" &
 exit
 fi
 
