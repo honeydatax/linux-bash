b654d552437796e969dc63d171e4d59bc9da12eb
honey data <honeydatax@sapo.pt>
2017-05-01 14:05:53 +0000

    free code list multiselect interface

--- tmenu.sh
+++ tmenu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-a=$(zenity --list --checklist --title="files" --separator="|" --column="pick" --column="select"   < list.txt)
+ls -l  > files.txt
+b=$(printf "\n")
+a=$(zenity --list --checklist --title="files" --separator="\n" --column="pick" --column="select" < files.txt )
 clear
 printf "\n%s\n" "$a"
 
