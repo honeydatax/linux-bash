d78eb718e079f70887222ef14f2fc149c82ddfd6
honey data <honeydatax@sapo.pt>
2017-05-01 23:41:08 +0000

    free code to set you data time

--- setDate.sh
+++ setDate.sh
@@ -1,41 +1,16 @@
 #!/bin/bash
 
-clear
+dialog --title "chage time clock" --timebox "change clock" 0 0 2> out.txt
+t=$(cat out.txt)
+sudo date +%T -s "$t"
+t=$(zenity --calendar --title="change date")
+printf "%s" "$t" > out.txt
+tr -d "-" < out.txt > out2.txt
+t=$(cat out2.txt)
+a=${t:4:4}${t:2:2}${t:0:2}
 
-printf "day:?"
-read tday
-tday=00$tday
-tday=${tday: -2}
+sudo date +%Y%m%d -s "$a"
 
-printf "month:?"
-read tmonth
-tmonth=00$tmonth
-tmonth=${tmonth: -2}
-
-
-printf "year:?"
-read tyear
-tyear=0000$tyear
-tyear=${tyear: -4}
-
-printf "hors:?"
-read thors
-thors=00$thors
-thors=${thors: -2}
-
-printf "minuts:?"
-read tminuts
-tminuts=00$tminuts
-tminuts=${tminuts: -2}
-
-tsec=00
-
-sudo date +%Y%m%d -s "$tyear$tmonth$tday"
-sudo date +%T -s "$thors:$tminuts:$tsec"
-
-date
-
-read nnull
 
 
 
