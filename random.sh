6fba03e91fef871995f291086e709b1f787d8c34
honey data <honeydatax@sapo.pt>
2017-05-01 22:24:50 +0000

    free code generator of random numbers

--- random.sh
+++ random.sh
@@ -2,4 +2,8 @@
 
 clear
 
-awk -F: 'BEGIN{srand();i=0;x=0;for (i=0;i<=11;i++){x=rand()*50+1;printf("%d \n",x);}}'
+awk -F: 'BEGIN{srand();i=0;x=0;for (i=0;i<=11;i++){x=rand()*50+1;printf("%d \n",x);}}' > out.txt 
+
+zenity --text-info --title="random numbers" --filename="out.txt" &
+
+
