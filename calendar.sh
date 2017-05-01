60dd6ddb0129a123c6bf4e9211f5f3620dda9352
honey data <honeydatax@sapo.pt>
2017-05-01 20:29:15 +0000

    free code utilitys to use  linux zenity interface

--- calendar.sh
+++ calendar.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 clear
 cal -y > cal.txt
-tr "_" " " < cal.txt > calender.txt
+tr -d "_" < cal.txt > calender.txt
 a=$(zenity --text-info --title="calandar.sh" --font="monospace" --filename="calender.txt" --width="600" --height="350") 
 
 
--- ccalc.sh
+++ ccalc.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
-
-printf ".....................\n"
-
-printf "info:?"
-read tinfo
-
-printf "value:?"
-read tvalue
+clear
+tinfo=$(zenity --entry --title="info:?" )
+tvalue=$(zenity --entry --title="value:?" )
 
 printf "%10s "  $tinfo >> ccalc.txt
 printf ":"  $tinfo >> ccalc.txt
 printf "%10s "  $tvalue >> ccalc.txt
 printf "\n"  >> ccalc.txt
 
-awk -F: 'BEGIN{value=0;}{value = value + $2 ; print $1 , "\t" , $2 , "\t" , value ; }END{print "total:\t",value}' ccalc.txt
-
-
-printf ".....................\n"
-
+awk -F: 'BEGIN{value=0;}{value = value + $2 ; print $1 , "\t" , $2 , "\t" , value ; }END{print "total:\t",value}' ccalc.txt > ucalc.txt
+printf ".....................\n" >> ucalc.txt
+zenity --text-info --title="ccalc.sh" --font="monospace" --filename="ucalc.txt" --width="600" --height="350"
 
 
 
