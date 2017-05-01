7f141f9342f0265ad85c9f13e77555ac6a0f6c59
honey data <honeydatax@sapo.pt>
2017-05-01 22:05:40 +0000

    free code utility to creat pdf files

--- editor.sh
+++ editor.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 x="10"
-y="750"
+y="850"
 f="16"
 var0="%PS"
 printf "%s\n" "$var0" > ps.ps
@@ -14,15 +14,11 @@ var7=") show"
 printf "%s\n%s %s\n%s\n%s\n%s %s %s\n%s%s\n" "$var1" "$f" "$var2" "$var3" "$var4" "$x" "$y" "$var5" "$var6" "$var7" >> ps.ps
 y=$(( y - f ))
 
-while :
+a=$(zenity --text-info --filename="out.txt" --editable --title="digit texto to pdf" )
+echo "$a" > out.txt
+cat out.txt | \
+while read var9 ;
 do
-
-read var9
-if [ "$var9" == "" ];
-then
-break
-fi
-
 y=$(( y - f ))
 printf "%s\n%s %s %s\n%s%s%s\n"  "$var4" "$x" "$y" "$var5" "$var6" "$var9" "$var7" >> ps.ps
 done
