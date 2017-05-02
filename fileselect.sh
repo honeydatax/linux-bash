f8cb13156cbd7b8cf8eea554144df2e676c8f668
honey data <honeydatax@sapo.pt>
2017-05-02 21:42:06 +0000

    free code text editor sample

--- fileselect.sh
+++ fileselect.sh
@@ -1,15 +1,16 @@
-#! /usr/bin/bash
-
-
-
+#!/bin/bash
+exits(){
+if [ "$1" = "1" ];
+then
+exit
+fi
+}
 
 a=$(zenity --file-selection --title="files" )
+b=$(zenity --text-info --title="file selected" --editable --filename="$a")
+exits $?
 
-clear
-
-
-
-printf "\n%s\n" "$a"
+echo "$b" > "$a"
 
 
 
