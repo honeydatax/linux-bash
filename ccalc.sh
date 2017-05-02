afbba835ff8856099ae2eebe8950d05cd56f2a5d
honey data <honeydatax@sapo.pt>
2017-05-02 11:31:17 +0000

    free code

--- ccalc.sh
+++ ccalc.sh
@@ -5,12 +5,12 @@ tvalue=$(zenity --entry --title="value:?" )
 
 printf "%10s "  $tinfo >> ccalc.txt
 printf ":"  $tinfo >> ccalc.txt
-printf "%10s "  $tvalue >> ccalc.txt
+printf "%10s"  $tvalue >> ccalc.txt
 printf "\n"  >> ccalc.txt
 
 awk -F: 'BEGIN{value=0;}{value = value + $2 ; print $1 , "\t" , $2 , "\t" , value ; }END{print "total:\t",value}' ccalc.txt > ucalc.txt
 printf ".....................\n" >> ucalc.txt
-zenity --text-info --title="ccalc.sh" --font="monospace" --filename="ucalc.txt" --width="600" --height="350"
+zenity --text-info --title="ccalc.sh delete ccalc.txt to stat over" --font="monospace" --filename="ucalc.txt" --width="600" --height="350"
 
 
 
