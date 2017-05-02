b3d50f7b87014e3c8aa803f82b2016b2d74b3d1a
honey data <honeydatax@sapo.pt>
2017-05-02 16:03:15 +0000

    free code a financial calculator

--- ccalc.sh
+++ ccalc.sh
@@ -3,9 +3,9 @@ clear
 tinfo=$(zenity --entry --title="info:?" )
 tvalue=$(zenity --entry --title="value:?" )
 
-printf "%10s "  $tinfo >> ccalc.txt
+printf "%10s " "$tinfo" >> ccalc.txt
 printf ":"  $tinfo >> ccalc.txt
-printf "%10s"  $tvalue >> ccalc.txt
+printf "%10s" "$tvalue" >> ccalc.txt
 printf "\n"  >> ccalc.txt
 
 awk -F: 'BEGIN{value=0;}{value = value + $2 ; print $1 , "\t" , $2 , "\t" , value ; }END{print "total:\t",value}' ccalc.txt > ucalc.txt
