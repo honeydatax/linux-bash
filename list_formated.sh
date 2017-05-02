3f716bd4f5f2951647a01f6e83c362519527512c
honey data <honeydatax@sapo.pt>
2017-05-02 20:46:03 +0000

    free code client list formated

--- list_formated.sh
+++ list_formated.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-clear
-awk -F: '{print $2,"\t",$1,"\t",$3}' client.txt
-read nnull
+awk -F: '{print $2,"\t",$1,"\t",$3}' client.txt > clients.txt
+zenity --text-info --title="formated list clients" --filename="clients.txt"
