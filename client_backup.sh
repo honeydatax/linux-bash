2271c49badcfaec8bdb2da9f78624ba80936fba4
honey data <honeydatax@sapo.pt>
2017-05-02 20:28:08 +0000

    free code database backup address book

--- client_backup.sh
+++ client_backup.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
-clear
 
 theDate=bak.$(date)
 mkdir "$theDate"
 
 cp client.txt "./$theDate/client.txt"
 
-chmod 777 "./$theDate/client.txt"
+chmod 666 "./$theDate/client.txt"
 
-printf "creat backup name:%s\n" "$theDate"
+printf "creat backup name:%s\n" "$theDate" > Date.txt 
+zenity --text-info --title="backup created." --filename="Date.txt"
 
 
-read nnull
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
