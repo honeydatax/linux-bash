2d7d786005a449097fb9973e7a48b3875f49283f
honey data <honeydatax@sapo.pt>
2017-05-03 17:47:10 +0000

    free code sample list of files

--- sfiles.sh
+++ sfiles.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 ls > list.txt
-printf "\n" >> tail.txt
+a=$(zenity --list --title="files" --separator=" " --column="files" < "list.txt")
 
-printf "\n" > line.txt
-line=$(cat line.txt)
-a=$(zenity --list --title="files" --separator=" " --column="files" < list.txt)
-
-
-clear
-
-
-printf "\n%s\n" "$a"
-
+zenity --warning --title="files selected" --text="$a"
 
 
 
