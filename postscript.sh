e516e83aa8b2dae5047fe98d8f0c85d62853ca89
honey data <honeydatax@sapo.pt>
2017-05-03 22:30:08 +0000

    free code post script sample

--- postscript.sh
+++ postscript.sh
@@ -1,58 +1,59 @@
-#!/usr/bin/bash
+#!/bin/bash
+y="780"
+
+opens(){
+ps2pdf "$1" "$2"
+xpdf "$2" &
+}
+
+head(){
+echo "%PS" > "$1"
+}
+
+tfonts(){
+echo "/$2 findfont" >> "$1"
+echo "$3 scalefont" >> "$1"
+echo "setfont" >> "$1"
+}
+
+tprint(){
+echo "newpath" >> "$1"
+echo "$2 $3 moveto" >> "$1"
+echo "($4) show" >> "$1"
+printf "%s : %s" "$3" "$5" > ccalc.txt
+y=$(awk -F: 'BEGIN{v=0;}{v = $1 - $2 ; print v ; }END{v = 0}' "ccalc.txt" )
+}
+
+
+file="ps.ps"
+fpdf="ps.pdf"
+ffont="Times-Roman"
+ssize="76"
 x="10"
-y="700"
-f="56"
-var0="%PS"
-printf "%s\n" "$var0" > ps.ps
-var1="/Times-Roman findfont"
-var2="scalefont"
-var3="setfont"
-var4="newpath"
-var5="moveto"
-var6="(im love marina"
-var7=") show"
-printf "%s\n%s %s\n%s\n%s\n%s %s %s\n%s%s\n" "$var1" "$f" "$var2" "$var3" "$var4" "$x" "$y" "$var5" "$var6" "$var7" >> ps.ps
-y=$(( y - f ))
-f="48"
-printf "%s\n%s %s\n%s\n%s\n%s %s %s\n%s%s\n" "$var1" "$f" "$var2" "$var3" "$var4" "$x" "$y" "$var5" "$var6" "$var7" >> ps.ps
-y=$(( y - f ))
-f="40"
-printf "%s\n%s %s\n%s\n%s\n%s %s %s\n%s%s\n" "$var1" "$f" "$var2" "$var3" "$var4" "$x" "$y" "$var5" "$var6" "$var7" >> ps.ps
-y=$(( y - f ))
-f="32"
-printf "%s\n%s %s\n%s\n%s\n%s %s %s\n%s%s\n" "$var1" "$f" "$var2" "$var3" "$var4" "$x" "$y" "$var5" "$var6" "$var7" >> ps.ps
-y=$(( y - f ))
-f="24"
-printf "%s\n%s %s\n%s\n%s\n%s %s %s\n%s%s\n" "$var1" "$f" "$var2" "$var3" "$var4" "$x" "$y" "$var5" "$var6" "$var7" >> ps.ps
-y=$(( y - f ))
-f="18"
-printf "%s\n%s %s\n%s\n%s\n%s %s %s\n%s%s\n" "$var1" "$f" "$var2" "$var3" "$var4" "$x" "$y" "$var5" "$var6" "$var7" >> ps.ps
-y=$(( y - f ))
-f="15"
-printf "%s\n%s %s\n%s\n%s\n%s %s %s\n%s%s\n" "$var1" "$f" "$var2" "$var3" "$var4" "$x" "$y" "$var5" "$var6" "$var7" >> ps.ps
-y=$(( y - f ))
-f="12"
-printf "%s\n%s %s\n%s\n%s\n%s %s %s\n%s%s\n" "$var1" "$f" "$var2" "$var3" "$var4" "$x" "$y" "$var5" "$var6" "$var7" >> ps.ps
-y=$(( y - f ))
-f="8"
-printf "%s\n%s %s\n%s\n%s\n%s %s %s\n%s%s\n" "$var1" "$f" "$var2" "$var3" "$var4" "$x" "$y" "$var5" "$var6" "$var7" >> ps.ps
-y=$(( y - f ))
-f="7"
-printf "%s\n%s %s\n%s\n%s\n%s %s %s\n%s%s\n" "$var1" "$f" "$var2" "$var3" "$var4" "$x" "$y" "$var5" "$var6" "$var7" >> ps.ps
-y=$(( y - f ))
-f="6"
-printf "%s\n%s %s\n%s\n%s\n%s %s %s\n%s%s\n" "$var1" "$f" "$var2" "$var3" "$var4" "$x" "$y" "$var5" "$var6" "$var7" >> ps.ps
-y=$(( y - f ))
-f="5"
-printf "%s\n%s %s\n%s\n%s\n%s %s %s\n%s%s\n" "$var1" "$f" "$var2" "$var3" "$var4" "$x" "$y" "$var5" "$var6" "$var7" >> ps.ps
-y=$(( y - f ))
-f="4"
-printf "%s\n%s %s\n%s\n%s\n%s %s %s\n%s%s\n" "$var1" "$f" "$var2" "$var3" "$var4" "$x" "$y" "$var5" "$var6" "$var7" >> ps.ps
+ttext="im love marina."
+
+head "$file"
+
+while :
+do
+ssize=$(( ssize - 10 ))
+tfonts "$file" "$ffont" "$ssize"
+tprint "$file" "$x" "$y" "$ttext" "$ssize"
+
+if [ "$ssize" = "36" ];
+then
+ffont="MONOSPACE"
+fi 
+
+if [ "$ssize" = "6" ];
+then
+break
+fi 
+done
 
 
-
-ps2pdf ps.ps ps.pdf
-xpdf ps.pdf &
+ 
+opens "$file" "$fpdf"
 
 
 
