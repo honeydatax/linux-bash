c62191fed24fd8c66412fcae82edc3dbaeb9eb23
honey data <honeydatax@sapo.pt>
2017-05-04 17:22:07 +0000

    free code sample equetion solver

---
+++ equation.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+a=$(zenity --entry --title="put vars and values?" --text="put var and values separete by ; sample: x=0;y=1")
+echo "$a" > equation.txt
+b=$(zenity --entry --title="put equetion?" --text="put equation sample: x * y")
+echo "$b" >> equation.txt
+a="x = 100 ; y = 3"
+b="x / y"
+c="{$a ; awk1000 = $b ; print awk1000 ; }"
+echo "0" | awk "$c" >> equation.txt
+echo "" >> equation.txt
+zenity --text-info --title="trying to solve..." --filename="equation.txt"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
