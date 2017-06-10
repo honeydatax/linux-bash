312f99ba82a19254c0897ea67e3f4e3b048adf25
honey data <honeydatax@sapo.pt>
2017-06-10 13:33:35 +0000

    free code id nodes of a tree object

---
+++ treeid.c
@@ -0,0 +1,204 @@
+#include <stdio.h>
+#include <termios.h>
+#include <string.h>
+#include <limits.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <math.h>
+
+
+struct node {
+node *nexts;
+node *container;
+int id;
+char text[50];
+};
+
+
+node *addNode(node *ns1,char *text);
+void addSubNode(node *ns1,char *text);
+node *creatList(char *text);
+void printList(node *ns1);
+
+
+void addStack(node **stacks,node *i);
+void startStack(node **stacks);
+int sizeStack();
+node *popStack(node **stacks);
+void subsub(int i);
+
+int idCount;
+int count=0;
+node *node0;
+
+
+
+int main(){
+int a=1;
+char s1[50];
+node *n1;
+node *n2;
+node *n3;
+system("clear");
+strcpy (s1,"title");
+node0=creatList(s1);
+for (int i=0;i<4;i++){
+sprintf(s1,"item %d",i);
+n1=addNode(node0,s1);
+for (int ii=0;ii<a;ii++){
+sprintf(s1,"sub item %d",ii);
+n2=addNode(n1,s1);
+for (int iii=0;iii<a;iii++){
+sprintf(s1,"sub sub item %d",iii);
+n3=addNode(n2,s1);
+for (int iiii=0;iiii<a;iiii++){
+sprintf(s1,"sub sub sub item %d",iiii);
+addNode(n3,s1);
+}
+}
+}
+a++;
+}
+printList(node0);
+subsub(20);
+printf("\n\ttotal nodes count : %d\n",idCount);
+return 0;
+}
+
+
+node *addNode(node *ns1,char *text){
+node *n1=new node;
+node *nl;
+node *nl2;
+strcpy(n1->text,text);
+n1->nexts=NULL;
+n1->container=NULL;
+n1->id=idCount;
+idCount++;
+nl=ns1->container;
+if (nl==NULL)ns1->container=n1;
+else{
+do{
+nl2=nl;
+nl=nl->nexts;
+}while(nl!=NULL);
+nl2->nexts=n1;
+}
+return n1;
+}
+
+
+
+node *creatList(char *text){
+node *ns1=new node;
+idCount=0;
+strcpy(ns1->text,text);
+ns1->nexts=NULL;
+ns1->container=NULL;
+ns1->id=idCount;
+idCount++;
+return ns1;
+}
+
+
+void printList(node *ns1){
+
+int i4=0;
+int i3=0;
+int i2=0;
+node *nl;
+node *nl1;
+node *nl2;
+node *stack[250];
+startStack(stack);
+printf("%s \t, id: %d\n",ns1->text,ns1->id);
+nl=ns1->container;
+if (nl!=NULL){
+do{
+i2=sizeStack();
+if (nl!=NULL){
+subsub(i2);
+printf("%s \t, id: %d\n\n",nl->text,nl->id);
+}
+if(nl->container!=NULL){
+addStack(stack,nl);
+nl=nl->container;
+}else{
+nl=nl->nexts;
+}
+i2=sizeStack();
+if (nl==NULL && i2>0){
+do{
+nl=popStack(stack);
+nl=nl->nexts;
+i4=0;
+if (nl!=NULL)i4=1;
+i2=sizeStack();
+if (i2<1)i4=1;
+}while(i4!=1);
+}
+i3=0;
+if (nl==NULL && i2<1)i3=1;
+}while(i3!=1);
+}
+}
+
+
+
+void addStack(node **stacks,node *i){
+count++;
+stacks[count]=i;
+}
+
+
+void startStack(node **stacks){
+count=0;
+}
+
+int sizeStack(){
+return count;
+}
+
+node *popStack(node **stacks){
+node *i=NULL;
+i=stacks[count];
+if (count<1)i=NULL;
+if (count>0)count--;
+return i;
+}
+
+
+void subsub(int i){
+for (int ii=0;ii<i+1;ii++)printf("--");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
