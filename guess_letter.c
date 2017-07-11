7624ba35d148b234f37f769c76cf7a31a6476ed8
honey data <honeydatax@sapo.pt>
2017-07-11 20:10:37 +0000

    free code guess a letter

---
+++ guess_letter.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <termios.h>
+#include <string.h>
+#include <limits.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <math.h>
+
+void order(int c,int *l,int **ll);
+void dorder(int c,int *l,int **ll);
+int nnumber(int n);
+int nremover(int c,int **ll);
+
+int main(){
+char s[8024];
+char *ss=&s[0];
+int nnnn=0;
+int nnn=0;
+unsigned char nn=0;
+int c=0;
+unsigned char b=0;
+int lens=15;
+int ll[1500];
+int *ll1=&ll[0];
+int *lll[1500];
+srandom(time(NULL));
+nn=(char)nnumber(25)+'a';
+nnn=0;
+system("clear");
+printf("game gess a letter\n");
+do{
+printf("gess a letter abcdefghijklmnopqrstuvwxyz  enter to give up\n");
+//scanf("%c",&b);
+ss=gets(ss);
+b=s[0];
+ll[c]=(int) b;
+c++;
+if (b>nn)printf("\nletter is to big\n");
+if (b<nn)printf("\nletter is to small\n");
+if(b==nn)nnn++;
+if(b<'a')nnn++;
+if(b>'z')nnn++;
+if(c>1498)nnn++;
+}while(nnn==0);
+order(c,ll1,lll);
+system("clear");
+for (int a=0;a<c;a++)printf ("%c = %c\n",ll[a],(int) lll[a]);
+if (b==nn)printf("you win = %c  try = %d\n",(char)nn,c);
+if (b!=nn)printf("you lost = %c  try = %d\n",(char)nn,c);
+return 0;
+}
+
+
+void order(int c,int *l,int **ll){
+int *d1,*d2,*d3,d4;
+if (c>0){
+for (int a=0;a<c;a++){
+if (a>0){
+d3=(int *) l[a];
+for (int b=0;b<a;b++){
+d1=ll[b];
+d2=d3;
+if (d1>d2){
+ll[b]=d2;
+d3=d1;
+}
+
+}
+ll[a]=d3;
+}else ll[0]=(int *)l[0];
+}
+}else ll[0]=(int *)l[0];
+}
+
+
+void dorder(int c,int *l,int **ll){
+int *d1,*d2,*d3,d4;
+if (c>0){
+for (int a=0;a<c;a++){
+if (a>0){
+d3=(int *) l[a];
+for (int b=0;b<a;b++){
+d1=ll[b];
+d2=d3;
+if (d1<d2){
+ll[b]=d2;
+d3=d1;
+}
+
+}
+ll[a]=d3;
+}else ll[0]=(int *)l[0];
+}
+}else ll[0]=(int *)l[0];
+}
+
+
+int nnumber(int n){
+int nnn=RAND_MAX/n;
+int nn=random()/nnn;
+return nn;
+}
+
+
+int nremover(int c,int **ll){
+int *d1,*d2,*d3,*d4;
+int rrnn=c;
+int cc=0;
+int back=-1;
+int ccc=0;
+for (int a=0;a<rrnn;a++){
+if(back==(int)ll[a]){
+if(a<rrnn){
+rrnn--;
+for (int b=a;b<rrnn;b++)ll[b]=ll[b+1];
+}
+}else back=(int)ll[a];
+}
+return rrnn;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
