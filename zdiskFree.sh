2129440353812ff27fdd4966e89188220b7a55be
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-08-31 22:08:08 +0100

    Delete zdiskFree.sh

--- zdiskFree.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-a=$(df)
-echo "$a" > out.txt
-
-zenity --text-info --title="gdi free disk" --text="gdi free disk" --width="600" --height="350" --filename="out.txt" --font="MONOSPACE"
- 
